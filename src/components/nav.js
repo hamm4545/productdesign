74e75953b72f1ec727fb528f85f2f5ed274d4666
billy0217 <billyh880217@gmail.com>
2020-07-07 22:14:12 +1200

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,14 +2,23 @@ import React from "react"
 import { Link } from "gatsby"
 import LogoImg from "../images/logo.svg"
 
+const isWorkPage = ({ location }) => {
+  return location.pathname == '/' ? { className: "selected" } : {}
+}
+
+const isAboutPage = ({ location }) => {
+  return location.pathname == '/about/' ? { className: "selected" } : {}
+}
+
 export default ({ children }) => (
-  <header>
+
+  <header className="content">
 	<Link to="/"><img src={LogoImg} alt="Heather Song" /></Link>
 	<ul class="nav">
-		<Link to="/" class="selected">Work</Link>
-		<Link to="/about/">About</Link>       
-	</ul> 
-    
+		<Link to="/" getProps={isWorkPage}>Work</Link>
+		<Link to="/about/" getProps={isAboutPage}>About</Link>
+	</ul>
+
     {children}
   </header>
 )
